53206580bc123af9bc5d61a1f9f54c690d9f0ff4
peikv <peikvincent@gmail.com>
2022-07-14 15:12:12 +0200

    tried to fix a problem

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,10 +13,14 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "MyTeam";
-           
-        } else { // TODO extend the programm here
-            return "";
+            return "MyTeam";
+
+        } else if (query.contains("%20what%20is%20")) {
+            //question %20what%20is%203%20plus%205
+            // TODO extend the programm here
+            String[] numbersToAdd = query.split("what%20is%20")[1].split("%20plus%20");
+            return "" + (Integer.parseInt(numbersToAdd[0]) + Integer.parseInt(numbersToAdd[1]));
         }
+        return "";
     }
 }
